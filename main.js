02d80178f875c5aee36b2e739b679949459c0f4b
Oghosamekhin-js <ogeguagie@gmail.com>
2025-02-05 17:36:11 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.querySelectorAll(".slide");
+
+  if (n >= slides.length) { slideIndex = 0; }
+  if (n < 0) { slideIndex = slides.length - 1; }
+
+  slides.forEach(slide => slide.style.display = "none");
+  slides[slideIndex].style.display = "block";
+}
